9ab2d5db11c02b784a49e7e23123ca8d64c37a8f
Göran Hegenberg <goeran@hegenberg-privat.de>
2026-09-02 23:06:41 +0200

    Install the dna_ggsuite DNA

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changelog
 
+## 4.3.0 - 2026-09-02
+
+### Changed
+
+- Install the dna_ggsuite DNA
+
 ## 4.2.0 - 2026-08-25
 
 ### Added
--- lib/src/gg_json_version.dart
+++ lib/src/gg_json_version.dart
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `gg_json` package.
-const String ggJsonVersion = '4.2.0';
+const String ggJsonVersion = '4.3.0';
